e087827db0670a479dc30851df1ab0e342c2b9ea
dlasap <lasapdominic@gmail.com>
2024-06-04 11:57:39 +0800

    add layout changes

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Lunar's Color Plotter" />
+        <meta property="og:description" content="Color plotter developed by Lunar" />
+        <meta property="og:image" content="https://cdn.britannica.com/70/191970-131-A85628DA/Color-wheel-light-color-spectrum.jpg" />
+        <title>Your App Title</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
